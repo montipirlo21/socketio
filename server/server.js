0cc64aab9f85d94f512b7a3cd05ffa05a9e6eadd
davide.monticelli <davide.monticelli@hotmail.it>
2018-09-26 17:48:21 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("New user connected");
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'A new user joined the chat',
+        createAt: new Date().getTime()
+    });
+
     //socket.emit('newEmail', {
     //    from: "davide.monticelli@hotmail.it",
     //    text: "Hello",
@@ -33,7 +45,14 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message) => {
         console.log('createMessage ', message);
 
-        io.emit('newMessage', {
+         io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
+
+        // A tutti tranne il socket
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createAt: new Date().getTime()
