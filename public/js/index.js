161320f42cb21fa922194954f3fafb0e4df8f650
davide.monticelli <davide.monticelli@hotmail.it>
2018-09-27 22:54:11 +0200

    scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 ﻿var socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    
+    // Heigth
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
+
 socket.on('connect', function () {
     console.log('connected to server');
     
@@ -23,6 +42,7 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     //var li = jQuery('<li></li>');
     //li.text(message.from + " " + formattedTime + ": " + message.text);
@@ -40,6 +60,7 @@ socket.on('newLocationMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 
 
     //var formattedTime = moment(message.createdAt).format('h:mm a');
