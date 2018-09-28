0153be28440a8b1e7a6fe408230e953d228c7bc5
davide.monticelli <davide.monticelli@hotmail.it>
2018-09-28 15:08:02 +0200

    inviare messaggi solo alla stanza a cui si partecipa

--- public/js/chat.js
+++ public/js/chat.js
@@ -95,7 +95,6 @@ jQuery('#message-form').on('submit', function (e) {
     var messageTextBox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextBox.val()
     },
         function () {
